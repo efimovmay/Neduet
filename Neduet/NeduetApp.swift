a058bf0caab714d8c480562345aaba566e6e473c
Aleksey Efimov <smaylik121@gmail.com>
2023-05-29 22:49:11 +0300

    Initial Commit

---
+++ Neduet/NeduetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeduetApp.swift
+//  Neduet
+//
+//  Created by Aleksey Efimov on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct NeduetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
